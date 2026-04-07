e9a3662135e07217c3abadbb4374b882f3e0d06b
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-04-07 11:37:44 +0900

    [add/#2] 컬러 시스템 추가

--- app/src/main/java/com/example/letssopt/ui/theme/Color.kt
+++ app/src/main/java/com/example/letssopt/ui/theme/Color.kt
@@ -1,11 +1,46 @@
 package com.example.letssopt.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryRed = Color(0xFFE8003C)
+val Background = Color(0xFF141414)
+val Surface = Color(0xFF2A2A2A)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF999999)
+val PlaceHolder = Color(0xFF666666)
+val Disabled = Color(0xFF333333)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Immutable
+data class LETSSOPTColors(
+    val primaryRed: Color,
+    val background: Color,
+    val surface: Color,
+    val txtPrimary: Color,
+    val txtSecondary: Color,
+    val placeholder: Color,
+    val disabled: Color,
+)
+
+val defaultLETSSOPTColors = LETSSOPTColors(
+    primaryRed = PrimaryRed,
+    background = Background,
+    surface = Surface,
+    txtPrimary = TextPrimary,
+    txtSecondary = TextSecondary,
+    placeholder = PlaceHolder,
+    disabled = Disabled,
+)
+
+val LocalLETSSOPTColors = staticCompositionLocalOf { defaultLETSSOPTColors }
