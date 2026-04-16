bd33bc6e31227dc01f28e59ae51ba4894935d1fb
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-04-16 12:24:20 +0900

    [feat/#4] 탑바 컴포넌트 구현

---
+++ app/src/main/java/com/example/letssopt/core/designsystem/component/LetsSoptTopBar.kt
@@ -0,0 +1,56 @@
+package com.example.letssopt.core.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.R
+import com.example.letssopt.core.designsystem.theme.LETSSOPTTheme
+
+@Composable
+fun LetsSoptTopBar(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 23.dp, horizontal = 20.dp),
+        horizontalArrangement = Arrangement.spacedBy(14.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_camera),
+            contentDescription = null,
+            tint = LETSSOPTTheme.colors.txtPrimary,
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_noti),
+            contentDescription = null,
+            tint = LETSSOPTTheme.colors.txtPrimary,
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_profile),
+            contentDescription = null,
+            tint = LETSSOPTTheme.colors.txtPrimary,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LetsSoptTopBarPreview(){
+    LETSSOPTTheme{
+        LetsSoptTopBar()
+    }
+}
