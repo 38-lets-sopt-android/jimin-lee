5f02657341b21a21e96504023a81822b401ae863
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-04-28 18:21:53 +0900

    [mod/#4] Immutable 어노테이션 추가

--- app/src/main/java/com/example/letssopt/presentation/main/MainContract.kt
+++ app/src/main/java/com/example/letssopt/presentation/main/MainContract.kt
@@ -1,11 +1,12 @@
 package com.example.letssopt.presentation.main
 
+import androidx.compose.runtime.Immutable
 import com.example.letssopt.presentation.main.component.MainTab
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 interface MainContract {
-
+    @Immutable
     data class State(
         val selectedItem: MainTab = MainTab.HOME,
         val items: ImmutableList<MainTab> = MainTab.entries.toImmutableList()
