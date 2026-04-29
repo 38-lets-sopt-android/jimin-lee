ec6f662bd3b35d26ca76fc29425ea41e727dce45
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-04-30 01:29:21 +0900

    [feat/#6] PurchaseItem 및 PurchaseScreen 구현

--- app/src/main/java/com/example/letssopt/presentation/purchase/PurchaseScreen.kt
+++ app/src/main/java/com/example/letssopt/presentation/purchase/PurchaseScreen.kt
@@ -1,32 +1,78 @@
 package com.example.letssopt.presentation.purchase
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.letssopt.R
 import com.example.letssopt.core.designsystem.theme.LETSSOPTTheme
+import com.example.letssopt.data.model.ContentItemModel
+import com.example.letssopt.presentation.purchase.component.PurchaseItem
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun PurchaseRoute(
     modifier: Modifier = Modifier,
+    viewModel: PurchaseViewModel = viewModel(),
 ) {
-    PurchaseScreen(modifier = modifier)
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    PurchaseScreen(
+        items = uiState.purchaseItems,
+        onSaveClick = { },
+        modifier = modifier,
+    )
 }
 
 @Composable
-private fun PurchaseScreen (
+private fun PurchaseScreen(
+    items: ImmutableList<ContentItemModel>,
+    onSaveClick: (Int) -> Unit,
     modifier: Modifier = Modifier,
-){
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center,
+) {
+    Column(
+        modifier = modifier
+            .padding(top = 70.dp)
+            .padding(horizontal = 16.dp),
     ) {
         Text(
-            text = "개별 구매 화면입니다.",
+            text = "개별 구매",
             color = LETSSOPTTheme.colors.txtPrimary,
+            style = LETSSOPTTheme.typography.h3.semibold20
         )
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(vertical = 10.dp),
+            horizontalArrangement = Arrangement.spacedBy(14.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+        ) {
+            items(
+                items = items,
+                key = { it.id }
+            ) { item ->
+                PurchaseItem(
+                    item = item,
+                    onSaveClick = { onSaveClick(item.id) },
+                )
+            }
+        }
     }
 }
 
@@ -34,6 +80,14 @@ private fun PurchaseScreen (
 @Composable
 private fun PurchaseScreenPreview() {
     LETSSOPTTheme {
-        PurchaseScreen()
+        PurchaseScreen(
+            items = persistentListOf(
+                ContentItemModel(1, R.drawable.img_home_1, "이 사랑 통역 되나요"),
+                ContentItemModel(2, R.drawable.img_home_2, "이상한일5"),
+                ContentItemModel(3, R.drawable.img_home_3, "하일매리"),
+                ContentItemModel(4, R.drawable.img_home_1, "이 사랑 통역 되나요"),
+            ),
+            onSaveClick = {},
+        )
     }
 }
---
+++ app/src/main/java/com/example/letssopt/presentation/purchase/component/PurchaseItem.kt
@@ -0,0 +1,82 @@
+package com.example.letssopt.presentation.purchase.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.R
+import com.example.letssopt.core.designsystem.theme.LETSSOPTTheme
+import com.example.letssopt.core.extension.noRippleClickable
+import com.example.letssopt.data.model.ContentItemModel
+
+private const val ASPECT_RATIO = 2/3f
+
+@Composable
+fun PurchaseItem(
+    item: ContentItemModel,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.width(IntrinsicSize.Min),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Box {
+            Image(
+                painter = painterResource(item.img),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(100.dp)
+                    .aspectRatio(ASPECT_RATIO)
+                    .clip(RoundedCornerShape(10.dp))
+            )
+
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_save),
+                contentDescription = null,
+                modifier = Modifier
+                    .noRippleClickable(onClick = onSaveClick)
+                    .align(Alignment.TopEnd)
+                    .padding(top = 6.dp, end = 6.dp),
+                tint = Color.Unspecified,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Text(
+            text = item.title,
+            color = LETSSOPTTheme.colors.txtPrimary,
+            style = LETSSOPTTheme.typography.body.regular16,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PurchaseItemPreview() {
+    LETSSOPTTheme {
+        PurchaseItem(
+            item = ContentItemModel(1, R.drawable.img_home_1, "이 사랑 통역 되나요"),
+            onSaveClick = {},
+        )
+    }
+}
