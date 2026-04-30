93828366c309c8319644c41106f970e8527e65f9
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-01 05:06:00 +0900

    [feat/#6] StorageEntity, StorageDao, AppDatabase 추가

---
+++ app/src/main/java/com/example/letssopt/data/local/dao/StorageDao.kt
@@ -0,0 +1,21 @@
+package com.example.letssopt.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.letssopt.data.local.entity.StorageEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StorageDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStorageItems(storageEntity: StorageEntity)
+
+    @Query("SELECT * FROM storage")
+    fun getStorageItems(): Flow<List<StorageEntity>>
+
+    @Delete
+    suspend fun deleteStorageItems(item: StorageEntity)
+}
