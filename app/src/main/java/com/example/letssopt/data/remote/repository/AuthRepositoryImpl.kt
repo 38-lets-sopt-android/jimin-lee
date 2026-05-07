cecf2aaedf43631ea86bce0caca0e698ac513ecb
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-07 15:45:19 +0900

    [feat/#8] login repository, impl 추가

--- app/src/main/java/com/example/letssopt/data/remote/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/letssopt/data/remote/repository/AuthRepositoryImpl.kt
@@ -1,15 +1,27 @@
 package com.example.letssopt.data.remote.repository
 
+import com.example.letssopt.core.utils.PreferencesUtil
 import com.example.letssopt.core.utils.suspendRunCatching
 import com.example.letssopt.data.remote.datasource.AuthRemoteDataSource
+import com.example.letssopt.data.remote.dto.auth.PostLoginRequestDto
+import com.example.letssopt.data.remote.dto.auth.PostLoginResponseDto
 import com.example.letssopt.data.remote.dto.auth.PostSignUpRequestDto
 
 class AuthRepositoryImpl (
+    private val preferencesUtil: PreferencesUtil,
     private val authRemoteDataSource: AuthRemoteDataSource,
 ) : AuthRepository {
 
     override suspend fun postSignUp(request: PostSignUpRequestDto): Result<Unit> =
         suspendRunCatching {
             authRemoteDataSource.postSignUp(request)
+        }
+
+    override suspend fun postLogin(request: PostLoginRequestDto): Result<PostLoginResponseDto> =
+        suspendRunCatching {
+            val response = authRemoteDataSource.postLogin(request).data ?: throw IllegalArgumentException("response data is null")
+            preferencesUtil.setAutoLogin(true)
+
+            response
         }
 }
