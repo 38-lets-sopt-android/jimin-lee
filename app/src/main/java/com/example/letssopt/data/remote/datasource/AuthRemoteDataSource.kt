c888184b9094af14de2f8739ce7ef0c7144d6a16
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-07 15:45:02 +0900

    [feat/#8] logindatasource, impl 추가

--- app/src/main/java/com/example/letssopt/data/remote/datasource/AuthRemoteDataSource.kt
+++ app/src/main/java/com/example/letssopt/data/remote/datasource/AuthRemoteDataSource.kt
@@ -1,8 +1,11 @@
 package com.example.letssopt.data.remote.datasource
 
 import com.example.letssopt.data.remote.dto.BaseResponse
+import com.example.letssopt.data.remote.dto.auth.PostLoginRequestDto
+import com.example.letssopt.data.remote.dto.auth.PostLoginResponseDto
 import com.example.letssopt.data.remote.dto.auth.PostSignUpRequestDto
 
 interface AuthRemoteDataSource {
     suspend fun postSignUp(request: PostSignUpRequestDto): BaseResponse<Unit>
+    suspend fun postLogin(request: PostLoginRequestDto): BaseResponse<PostLoginResponseDto>
 }
--- app/src/main/java/com/example/letssopt/data/remote/datasource/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/letssopt/data/remote/datasource/AuthRemoteDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.example.letssopt.data.remote.datasource
 
 import com.example.letssopt.data.remote.dto.BaseResponse
+import com.example.letssopt.data.remote.dto.auth.PostLoginRequestDto
+import com.example.letssopt.data.remote.dto.auth.PostLoginResponseDto
 import com.example.letssopt.data.remote.dto.auth.PostSignUpRequestDto
 import com.example.letssopt.data.remote.service.AuthService
 
@@ -10,6 +12,10 @@ class AuthRemoteDataSourceImpl (
 
     override suspend fun postSignUp(request: PostSignUpRequestDto): BaseResponse<Unit> {
         return authService.postSignUp(request = request)
+    }
+
+    override suspend fun postLogin(request: PostLoginRequestDto): BaseResponse<PostLoginResponseDto> {
+        return authService.postLogin(request = request)
     }
 
 }
